4e63eb42438c47c3fddd7ce0470bad51a307e928
Tallys-aureliano <tallysaureliano@outlook.com>
2023-03-02 17:21:09 -0300

    :tada: Inicial Project

---
+++ Target-Java/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+    }
+}
